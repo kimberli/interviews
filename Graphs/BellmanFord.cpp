4a1778655247287a604e9b9776f4a52df30cf043
SharatMehrotra <47215289+SharatMehrotra@users.noreply.github.com>
2021-06-10 11:49:27 +0530

    Update BellmanFord.cpp

--- Graphs/BellmanFord.cpp
+++ Graphs/BellmanFord.cpp
@@ -5,17 +5,17 @@ vector<int> BellmanFord(int V,int src,vector<vector<int>>edges){   // {src,dest,
     dist[src]=0;                                                  //dist from src will be zero
     for (int i = 1; i < V; i++) {
         for (int j = 0; j < E; j++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int weight = edges[i][2];
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int weight = edges[j][2];
             if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
                 dist[v] = dist[u] + weight;
         }
     }
   for(int i=0; i < E; i++){
-    int u=edges[j][0];
-    int v=edges[j][1];
-    int w=edges[j][2];
+    int u=edges[i][0];
+    int v=edges[i][1];
+    int w=edges[i][2];
     if(dist[u] != INT_MAX && dist[u] + w <dist[v]){
       return {};                                            // there is a negative cycle
     }
